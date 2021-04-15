99713dedb70fe916ae68d0a8f6be156193c90acc
7EBPrescott <7EBPrescott@helston.cornwall.sch.uk>
2021-04-15 11:52:08 +0100

    Clean up StoryTen code

--- src/StoryTen/StoryTen.ino
+++ src/StoryTen/StoryTen.ino
@@ -1,94 +1,31 @@
 #include <NewPing.h>
 
+// Pin definitions
 int fwdright = 12;
 int fwdleft = 8;
 int revright = 11;
 int revleft = 7;
 int enableleft = 9;
 int enableright = 10;
-//Array of pins
+
+// Array of pins
 int vars[6] = {fwdright,fwdleft,revright,revleft,enableleft,enableright};
 
-const int leftfeedback = 2;//Switch pins
+const int leftfeedback = 2; // Switch pins
 const int rightfeedback = 3;
+
 volatile int leftcounter = 0;
 volatile int rightcounter = 0;
-const int reduc = 50;
-int count = 0;
-
 
 const int startspeed = 150;
-int leftspeed; 
-int rightspeed;
+int leftspeed = startspeed; 
+int rightspeed = startspeed;
 
 const int TRIGGER_DOWN = 13;
 const int ECHO_DOWN = 17;
 const int MAX_DISTANCE = 200; // Maximum distance we want to ping for (in centimeters).
 
 NewPing sonar(TRIGGER_DOWN, ECHO_DOWN, MAX_DISTANCE);
-
-void halt(int mSeconds){ //stopps rover advance
-  
-  digitalWrite(fwdleft,LOW);
-  digitalWrite(fwdright,LOW);
-  digitalWrite(revleft,LOW);
-  digitalWrite(revright,LOW);
-  delay(mSeconds);
-}
-
-void reverse(int mSeconds){ //stop motor forward, rover reverses
-  
-  digitalWrite(fwdleft,LOW);
-  digitalWrite(fwdright,LOW);
-  digitalWrite(revleft,HIGH);
-  digitalWrite(revright,HIGH);
-  delay(mSeconds);
-}
-
-void forward(int mSeconds) {//stop motor reverse, rover forward
-  
-  digitalWrite(revleft,LOW);
-  digitalWrite(revright,LOW);
-  digitalWrite(fwdleft,HIGH);
-  digitalWrite(fwdright,HIGH);
-  delay(mSeconds);
-  
-}
-
-void turnright(int mSeconds) {//right turn by motor speed change
-  analogWrite(enableleft, 255);
-  analogWrite(enableright, 120);
-  delay(mSeconds);
-}
-
-void turnleft(int mSeconds)  {//left turn by motor speed change
-  analogWrite(enableright, 255);
-  analogWrite(enableleft, 120);
-  delay(mSeconds);
-}
-
-void updateSpeeds(){
-  analogWrite(enableleft,rightspeed);
-    analogWrite(enableright,leftspeed);
-}
-
-void setup() {
-  for(int v : vars){
-    pinMode(v,OUTPUT);
-  }
-  
-  Serial.begin(115200);
-  attachInterrupt(digitalPinToInterrupt(leftfeedback), lefttracker, RISING);
-  attachInterrupt(digitalPinToInterrupt(rightfeedback), righttracker, RISING);
-  
-  leftspeed = startspeed;
-  rightspeed = startspeed;
-  
-  analogWrite(enableleft, rightspeed);
-  analogWrite(enableright, leftspeed); 
-  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
-  sonar.ping_cm();
-}
 
 void setHigh(int pin) {
   digitalWrite(pin, HIGH);
@@ -98,27 +35,86 @@ void setLow(int pin) {
   digitalWrite(pin, LOW);
 }
 
-/*void turnLeft90() {
-  leftcounter = 0;
+void halt(int mseconds){ // Stops rover advance
+  setLow(fwdleft);
+  setLow(fwdright);
+  setLow(revleft);
+  setLow(revright);
+  delay(mseconds);
+}
 
-  while (leftcounter <= 85) {
-    setHigh(enableleft);
-    setHigh(enableright);
-    
-    setLow(fwdleft);
-    setHigh(fwdright);
-    
-    setHigh(revleft);    
-    setLow(revright);
+void reverse(int mseconds){ // Stop motor forward, rover reverses
+  setLow(fwdleft);
+  setLow(fwdright);
+
+  setHigh(revleft);
+  setHigh(revright);
+  delay(mseconds);
+}
+
+void forward(int mseconds) { // Stop motor reverse, rover forward
+  setLow(revleft);
+  setLow(revright);
+  
+  setHigh(fwdleft);
+  setHigh(fwdright);
+  delay(mseconds);
+  
+}
+
+void turnLeft(int mseconds)  { // Left turn by motor speed change
+  analogWrite(enableright, 255);
+  analogWrite(enableleft, 120);
+  delay(mseconds);
+}
+
+void turnRight(int mseconds) { // Right turn by motor speed change
+  analogWrite(enableleft, 255);
+  analogWrite(enableright, 120);
+  delay(mseconds);
+}
+
+void updateSpeeds(){
+  analogWrite(enableleft,rightspeed);
+  analogWrite(enableright,leftspeed);
+}
+
+// Increment left and right counters
+void leftTracker(){
+  leftcounter++;
+}
+
+void rightTracker(){
+  rightcounter++; 
+}
+
+void setup() {
+  for(int v : vars){
+    pinMode(v, OUTPUT);
   }
+  
+  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+  
+  attachInterrupt(
+    digitalPinToInterrupt(leftfeedback), 
+    leftTracker, 
+    RISING
+  );
+   
+  attachInterrupt(
+    digitalPinToInterrupt(rightfeedback), 
+    rightTracker, 
+    RISING
+  );
+  
+  updateSpeeds();
+  sonar.ping_cm();
+}
 
-  leftcounter = 0;
-}*/
-
-//Return true if there is an obstacle in the path
+// Return true if there is an obstacle in the path
 bool objectBelow(){
   int distance = sonar.ping_cm();
-  return distance <= 12; //If there is an obstacle 12cm or less ahead
+  return distance <= 12; // If there is an obstacle 12cm or less ahead
 }
 
 void loop() {
@@ -130,12 +126,4 @@ void loop() {
   }
   
   delay(50);
-}
-//Increment left and right counters
-void lefttracker(){
-  leftcounter++;
-}
-
-void righttracker(){
-  rightcounter++; 
 }
